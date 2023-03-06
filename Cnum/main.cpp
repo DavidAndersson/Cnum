dc5e30f8f351b8c9d0dc16b6cbde78b27f4942bd
DAvid <david.eg.andersson@gmail.com>
2023-03-06 19:51:07 +0100

    New version of Transpose for higher orders, as well as unit test capabillities added

--- Cnum/main.cpp
+++ Cnum/main.cpp
@@ -1,8 +1,5 @@
-#include "kdTree.h"
-#include "Rect.h"
-#include "DynamicArray.h"
 #include "Cnum.h"
-#include "Rect.h"
+#include <iostream>
 
 // Make the priority scheme for Rect overlapping more explicit
 
@@ -10,7 +7,12 @@
 int main() {
 
 
-	fArray test = DynamicArray<float>();
+	dArray arr = Cnum::Array<double>({ 0.55, 0.35, 0.87, 0.85, 0.13, 0.54, 0.67, 0.41 }, { 2,2,2 });
+	dArray arrT = arr.Transpose({2, 1, 0});
+	dArray answer = Cnum::Array<double>({ 0.55, 0.13, 0.87, 0.67, 0.35, 0.54, 0.85, 0.41 }, { 2,2,2 });
+	bool isSame = arrT == answer;
+
+	arrT.Print();
 
 
 }
---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Cnum/DynamicArray.h"
+#include "../Cnum/Cnum.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(Test_Transpose)
+		{
+			dArray arr = Cnum::Array<double>({ 0.47, 0.40, 0.08, 0.12, 0.21, 0.72, 0.62, 0.85 }, { 2,2,2 });
+			dArray arrT = arr.Transpose();
+			dArray answer = Cnum::Array<double>({ 0.47, 0.21, 0.08, 0.62, 0.4, 0.72, 0.12, 0.85}, { 2,2,2 });
+			bool isSame = arrT == answer;
+			Assert::IsTrue(isSame);
+
+		}
+	};
+}
